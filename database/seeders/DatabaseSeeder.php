af5f8b4d944cc636f0fe0881aa42edc872200bce
Davy <davy@datalink.be>
2025-12-02 16:34:30 +0100

    🔧 chore: add article and user factory to DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\ArticleComment;
+use App\Models\ArticleImage;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +19,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin@example.com'),
         ]);
+
+        User::factory(10)->create();
+
+        for ($i=0; $i <32; $i++) { 
+            Article::factory()->create([
+                'user_id' => User::inRandomOrder()->first()
+            ]);
+        }
+
+        for ($i=0; $i <100; $i++) { 
+            ArticleComment::factory()->create([
+                'user_id' => User::inRandomOrder()->first(),
+                'article_id' => Article::inRandomOrder()->first()
+            ]);
+
+        }
+
+
+        for ($i=0; $i < 256; $i++) { 
+            ArticleImage::factory()->create([
+                'article_id' => User::inRandomOrder()->first()
+            ]);
+        }
     }
 }
