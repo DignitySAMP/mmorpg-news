fa58c96816da24ea0dcab23aa15ea847cc306155
Davy <davy@datalink.be>
2025-12-02 17:00:53 +0100

    📝 docs(todo): add filter directives

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,20 @@ class HomeController extends Controller
 {
     public function index(Request $request) {
 
+        // TODO: add filter for article
+        /*
+
+            - newest to oldest
+            - oldest to newest
+            - author
+            - title
+            - text
+
+            TODO: add article tags
+            -> make it so these are dynamically created, appended or deleted if no articles are using them
+
+        */
+
         return Inertia::render('Welcome', [
             'articles' => Article::with(['user', 'comments', 'images'])->get()->append(['similar_articles', 'read_time'])
         ]);
