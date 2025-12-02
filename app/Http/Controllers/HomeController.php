9e94473cd1f2018843c2a3293f69dd10efab4d09
Davy <davy@datalink.be>
2025-12-02 16:49:41 +0100

    ✨ feat: add HomeController, point it to 'Welcome' in web.php, clean up Welcome.vue

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    public function index(Request $request) {
+
+        return Inertia::render('Welcome', [
+            'articles' => Article::with('user', 'comments', 'images')->get()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
