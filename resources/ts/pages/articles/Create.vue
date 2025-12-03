a5e2d13303723c9e1829a898f65cf7da5bb31e34
Davy <davy@datalink.be>
2025-12-03 10:04:28 +0100

    🕰️ wip: add md-editor-v3

---
+++ resources/ts/pages/articles/Create.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppTempLayout from '@/layouts/AppTempLayout.vue';
+import { ref } from 'vue';
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('# Hello Editor');
+
+
+const onSave = (v:string, h: Promise<string>) => {
+  console.log(v);
+
+  h.then((html: string) => {
+    console.log('html:',html);
+  });
+};
+
+</script>
+
+<template>
+    <AppTempLayout>
+        <MdEditor @onSave="onSave" v-model="text" language="en-US" :noKatex="true" :noMermaid="true" :noUploadImg="true" :toolbarsExclude="['mermaid']" />
+    </AppTempLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ use Inertia\Inertia;
 
 Route::get('/', [ArticleController::class, 'index'])->name('article.index');
 
+Route::get('/article/create', function() {
+    return Inertia::render('articles/Create');
+})->name('article.create');
+
 // TODO: make show article part of the slug, which should be editable in article editor
