9b3cbc6c43f8437cc8580ccd2ca38a4535081d85
Davy <davy@datalink.be>
2025-12-03 11:39:19 +0100

    🌱 init: install Fortify and configure Login, Register and ForgotPassword

---
+++ resources/ts/pages/Fortify/ForgotPassword.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Form } from '@inertiajs/vue3'
+import { login } from '@/wayfinder/routes';
+import { email } from '@/wayfinder/routes/password';
+
+defineProps({
+  status: String
+})
+</script>
+
+<template>
+    <span>
+      Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+    </span>
+
+    <Form :action="email()" #default="{
+		errors,
+		hasErrors,
+		processing,
+		recentlySuccessful,
+		wasSuccessful
+	}">
+		<span v-if="recentlySuccessful || wasSuccessful">
+			We have emailed you a password reset link.
+		</span>
+        <div class="flex flex-col gap-1">
+          	<label for="email">Email</label>
+          	<input class="border" type="email" name="email" autocomplete="email"/>
+          	<span v-if="hasErrors && errors.email" v-html="errors.email"/>
+        </div>
+
+		<div class="flex flex-col gap-1">
+			<button class="border" type="submit" :disabled="processing">
+				Email Password Reset Link
+			</button>
+
+			<a class="underline" :href="login().url">
+				Remember your password?
+			</a>
+		</div>
+    </Form>
+</template>
---
+++ resources/ts/pages/Fortify/Login.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { Form } from '@inertiajs/vue3'
+import { register } from '@/wayfinder/routes';
+import { store } from '@/wayfinder/routes/login';
+import { request } from '@/wayfinder/routes/password';
+
+defineProps({
+  status: String
+})
+
+</script>
+
+<template>
+	<div>
+		<span v-if="status">
+			{{ status }}
+		</span>
+
+		<Form :action="store()" #default="{
+			errors,
+			hasErrors,
+			processing,
+		}">
+			<div class="flex flex-col gap-1">
+				<label for="email">Email</label>
+				<input class="border" type="email" name="email" autocomplete="email"/>
+				<span v-if="hasErrors && errors.email" v-html="errors.email"/>
+			</div>
+			<div class="flex flex-col gap-1">
+				<label for="password">Password</label>
+				<input class="border" type="password" name="password" autocomplete="current-password"/>
+				<span v-if="hasErrors && errors.password" v-html="errors.password"/>
+			</div>
+			<div class="flex gap-1">
+				<input type="checkbox" name="remember">
+				<label for="remember">Remember me</label>
+			</div>
+
+			<div class="flex flex-col gap-1">
+				<button class="border" type="submit" :disabled="processing">
+					Login
+				</button>
+
+				<a class="underline" :href="register().url">
+					Register account
+				</a>
+				<a class="underline" :href="request().url">
+					Forgot your password?
+				</a>
+			</div>
+		</Form>
+  	</div>
+</template>
