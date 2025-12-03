ba47ef8bba1e28e5189bfd7486043ace5a8144b4
Davy <davy@datalink.be>
2025-12-03 09:16:39 +0100

    ✨ feat: implement filtering using back-end parables on front-end

--- resources/ts/pages/Welcome.vue
+++ resources/ts/pages/Welcome.vue
@@ -1,8 +1,10 @@
 <template>
     <main>
-        <select v-model="sortOption">
+        <select v-model="sortBy">
             <option v-for="options in sort_options" :value="options.value" v-html="options.label"/>
         </select>
+        <input type="text" v-model="search" placeholder="Enter title or description here"/>
+        <input type="range" min="5" :max="articles.total" v-model="perPage"/>
         <div class="grid grid-cols-4 gap-4">
             <div v-for="article in articles.data">
                 <img v-if="article.banner" :src="article.banner"/>
@@ -21,7 +23,10 @@
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
+    import { router } from '@inertiajs/vue3';
+    import { debounce } from 'lodash';
+    import { index } from '@/wayfinder/routes/article';
     import { type Article } from '@/types/article'
 
 
@@ -38,6 +43,7 @@
         filters: {
             sort_by: string;
             per_page: number;
+            search: string;
         };
         sort_options: {
             value: string;
@@ -46,6 +52,28 @@
     }
 
     const props = defineProps<WelcomePageProps>();
-    const sortOption = ref<string>(props.sort_options[0].value);
+
+    // local models for search filtering
+    const sortBy = ref(props.filters.sort_by);
+    const search = ref(props.filters.search);
+    const perPage = ref(props.filters.per_page);
+
+    // fetch results from article.index based on filter parameters
+    const onFilterArticles = () => {
+        router.get(index(), {
+            search: search.value,
+            sort_by: sortBy.value,
+            per_page: perPage.value,
+        }, {
+            preserveState: true,
+            preserveScroll: true,
+            replace: true,
+        });
+    }
+    watch([sortBy, perPage], () => onFilterArticles()); // sortby and perpage should be instanteneous for good user UX
+    const debouncedSearch = debounce(() => onFilterArticles(), 300); //  create a debounce instance for input strings
+    watch(search, () => debouncedSearch()); // debounce all input strings by 300-500ms for performance
+
+
 
 </script>
