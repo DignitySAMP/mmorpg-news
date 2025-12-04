9674963dcd812649222babb789f31801078959ac
Davy <davy@datalink.be>
2025-12-04 09:30:33 +0100

    🔧 chore: implement AppCountrySelect into Profile/Edit

--- resources/ts/pages/Profile/Edit.vue
+++ resources/ts/pages/Profile/Edit.vue
@@ -1,5 +1,13 @@
 <template>
-    <span>
-        Edit location, dob, gender, privacy, ...
-    </span>
+    <span> Edit location, dob, gender, privacy, ... </span>
+
+    <div class="w-lg">
+        <AppCountrySelector v-model="country" />
+    </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppCountrySelector from '@/components/AppCountrySelector.vue';
+
+const country = ref<string>("");
+</script>
